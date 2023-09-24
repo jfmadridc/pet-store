a2290385d978e7f7bd3acd39ee3a84df4cedcc6b
JuanF Madrid <jfm@jfmadrid.com>
2023-09-23 19:17:34 -0500

    Primera manera de hacer el routing segun la url

--- src/App.js
+++ src/App.js
@@ -1,12 +1,21 @@
-import './assets/css/base/base.css';
-import './assets/css/componentes/card.css'
+import "./assets/css/base/base.css";
+import "./assets/css/componentes/card.css";
+import Home from "./pages/Home";
+import Sobre from "./pages/Sobre";
 
 function App() {
-  return (
-    <>
+	const location = window.location;
+	console.log(location.pathname);
 
-    </>
-  );
+	const Router = () => {
+		if (location.pathname === "/") {
+			return <Home />;
+		} else {
+			return <Sobre />;
+		}
+	};
+
+	return <>{Router()}</>;
 }
 
 export default App;
