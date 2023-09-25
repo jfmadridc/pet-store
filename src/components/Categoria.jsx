2b24c4d73157f4c4b983bfbd29d89631cd31e573
JuanF Madrid <jfm@jfmadrid.com>
2023-09-25 13:54:04 -0500

    Added subcategory list rendering
    added functionality to render a list of subcategories. It fetches the subcategories based on the provided ID using the 'buscar' API function. The retrieved subcategories are then mapped and displayed as clickable links.

--- src/components/Categoria.jsx
+++ src/components/Categoria.jsx
@@ -1,16 +1,43 @@
 import "../assets/css/blog.css";
 import ListCategories from "../components/ListCategories";
 import ListPosts from "../components/ListPosts";
+import { buscar } from "../api/api";
 import { useParams, Routes, Route } from "react-router-dom";
+import { useEffect, useState } from "react";
+import { Link } from "react-router-dom";
+import { useResolvedPath } from "react-router-dom";
 
 const Categoria = () => {
 	const { id } = useParams();
+	const [subcategorias, setSubcategorias] = useState([]);
+
+	const url = useResolvedPath("").pathname;
+
+	useEffect(() => {
+		buscar(`/categorias?id=${id}`, (respuesta) => {
+			setSubcategorias(respuesta[0].subcategorias);
+		});
+	}, [id]);
+
 	return (
 		<>
 			<div className="container">
 				<h2 className="title-page">Pet Noticias</h2>
 			</div>
+
 			<ListCategories />
+
+			<ul className="category-list container flex">
+				{subcategorias.map((subcategoria) => (
+					<li
+						className={`category-list__category category-list__category--${id}`}
+						key={subcategoria}
+					>
+						<Link to={`${url}/${subcategoria}`}>{subcategoria}</Link>
+					</li>
+				))}
+			</ul>
+
 			<Routes>
 				<Route path="/" element={<ListPosts url={`/posts?categoria=${id}`} />} />
 			</Routes>
